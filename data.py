4a72021ff59f3d45006b53d9200fe71d724faef4
Rickard Malmström <rickardmalmstrom@gmail.com>
2018-02-28 11:12:52 +0100

    Init

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio alias consequatur veritatis? Qui, expedita omnis! Voluptatum corporis illum commodi suscipit sapiente ut ullam architecto at. Blanditiis quasi sit quaerat odit!',
+            'author':'Rickard Malmström',
+            'create_date':'15-02-2018'
+        },
+        {
+            'id':2,
+            'title':'Article Two',
+            'body':'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio alias consequatur veritatis? Qui, expedita omnis! Voluptatum corporis illum commodi suscipit sapiente ut ullam architecto at. Blanditiis quasi sit quaerat odit!',
+            'author':'Johanne Anthonisen',
+            'create_date':'15-02-2018'
+        },
+        {
+            'id':3,
+            'title':'Article three',
+            'body':'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio alias consequatur veritatis? Qui, expedita omnis! Voluptatum corporis illum commodi suscipit sapiente ut ullam architecto at. Blanditiis quasi sit quaerat odit!',
+            'author':'Rickard Malmström',
+            'create_date':'15-02-2018'
+        }
+    ]
+    return articles
